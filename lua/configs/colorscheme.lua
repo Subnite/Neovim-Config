27af1ff97289dab289df4c75e7176eb9b3329160
Subnite <vanrietilias@hotmail.com>
2025-02-09 00:39:51 +0100

    updated theme to fix breaking changes.

--- lua/configs/colorscheme.lua
+++ lua/configs/colorscheme.lua
@@ -49,7 +49,7 @@ require("cyberdream").setup({
     hide_fillchars = false,
 
     -- Modern borderless telescope theme - also applies to fzf-lua
-    borderless_telescope = false,
+    borderless_pickers = false,
 
     -- Set terminal colors used in `:terminal`
     terminal_colors = true,
@@ -57,7 +57,7 @@ require("cyberdream").setup({
     -- Improve start up time by caching highlights. Generate cache with :CyberdreamBuildCache and clear with :CyberdreamClearCache
     cache = false,
 
-    theme = {
+    opts = {
         variant = "default", -- use "light" for the light variant. Also accepts "auto" to set dark or light colors based on the current value of `vim.o.background`
         saturation = 1, -- accepts a value between 0 and 1. 0 will be fully desaturated (greyscale) and 1 will be the full color (default)
         highlights = {
